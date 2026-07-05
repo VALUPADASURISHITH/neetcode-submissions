c88ed447a3e5d7e5659202634c77c4096b4d667b
Rishith <2400030703@kluniversity.in>
2026-07-05 11:55:02 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mp=0;
+        int bb=prices[0];
+        for(int i=1;i<prices.length;i++){
+            mp=Math.max(mp,prices[i]-bb);
+            bb=Math.min(bb,prices[i]);
+        }
+        return mp;
+    }
+}
